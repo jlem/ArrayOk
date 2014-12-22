7dc4e672fd3e6d2a964e177b97732c2677309c72
Jon <jlemaitre@theatomgroup.com>
2014-12-22 17:35:13 -0500

    ensuring compatibility with 5.3

--- src/ArrayOk.php
+++ src/ArrayOk.php
@@ -318,7 +318,7 @@ class ArrayOk implements \ArrayAccess
     protected function getRecursively($keys)
     {
         return array_reduce($this->normalizeKeys($keys), function($carry, $item) {
-            return ($this->isAok($carry)) ? $carry->getSingle($item) : null;
+            return ($carry instanceof ArrayOk)) ? $carry->getSingle($item) : null;
         }, $this);
     }
 
